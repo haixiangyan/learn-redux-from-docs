fc42747245be0172872d92e14a2b05c2f4c3623a
Haixiang <haixiang6123@gmail.com>
2020-09-03 02:28:03 +0800

    Updates

---
+++ precache-manifest.0f48d0c7de49ea4c46c73a62c65c1a71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f3f0d0ce57dd9a22f8a4f33d178c842",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f706fab7089916b539c8",
+    "url": "/static/css/2.847e01e0.chunk.css"
+  },
+  {
+    "revision": "0428fbdb94ebd07b5af4",
+    "url": "/static/css/main.c75ec9ac.chunk.css"
+  },
+  {
+    "revision": "f706fab7089916b539c8",
+    "url": "/static/js/2.0def0670.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0def0670.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0428fbdb94ebd07b5af4",
+    "url": "/static/js/main.de676dda.chunk.js"
+  },
+  {
+    "revision": "e8bee4074c59fd3a4eb4",
+    "url": "/static/js/runtime-main.7b7b4dc9.js"
+  }
+]);
